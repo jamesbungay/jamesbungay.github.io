92c19df3119fe1fb0bfee6d1f37147ab50cf4109
James Bungay <jtb.github@gmail.com>
2020-10-18 17:32:18 +0100

    Updates

---
+++ precache-manifest.aa17db7c5dec28401a299f069490e466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d49df326f907061ed6e62471ee85a03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c917866138aa61eb00",
+    "url": "/static/css/main.3dfc0f80.chunk.css"
+  },
+  {
+    "revision": "dc44428422a8b13ecfbb",
+    "url": "/static/js/2.4b602b01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b602b01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c917866138aa61eb00",
+    "url": "/static/js/main.d282b7af.chunk.js"
+  },
+  {
+    "revision": "b830aae7af994c353bc6",
+    "url": "/static/js/runtime-main.bfd1baec.js"
+  },
+  {
+    "revision": "434b53e302b70ce4b8e21bd8bc609c3a",
+    "url": "/static/media/portfolio_carboncamera.434b53e3.png"
+  },
+  {
+    "revision": "d0bd6b514898dfc2c377f0b10cb05c67",
+    "url": "/static/media/social_linkedin.d0bd6b51.png"
+  }
+]);
