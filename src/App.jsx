a616e0a9810f7d44cc1ba63beb0ab23519ce1989
James Bungay <jtb.github@gmail.com>
2021-03-27 20:12:05 +0000

    Add Google analytics code to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,12 +7,18 @@ import {
   Redirect
 } from 'react-router-dom';
 
+import RouteChangeTracker from './components/RouteChangeTracker';
+
 import Portfolio from './routes/Portfolio';
 import NotFound from './routes/NotFound';
+
+const TRACKING_ID = "UA-193123795-1";
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
+      <RouteChangeTracker />
       <Switch>
         <Route path="/" exact>
           <Portfolio />
