e02e751c042de4146b88ae5d3f0ff621d8b17650
James Bungay <jtb.github@gmail.com>
2024-03-26 22:22:37 +0000

    Update portfolio

--- src/routes/Portfolio/Portfolio.jsx
+++ src/routes/Portfolio/Portfolio.jsx
@@ -17,6 +17,30 @@ const Portfolio = () => {
         bodyContent={
           <div>
             <p className="Text-SubTitle">
+            Contactless Hand Tremor Amplitude Measurement using Smartphones
+            </p>
+            <p className="Text-Paragraph">
+            A paper summarising my university dissertation, where I developed a method to measure the amplitude of Parkinson's hand tremors. Tremor amplitude is measured using a computer vision analysis of data from a smartphone's built in depth sensor and camera.
+            </p>
+            <p className="Text-Paragraph">
+            The paper was subsequently published and accepted to the 2023 Conference of the IEEE Engineering in Medicine and Biology Society.
+            </p>
+            <a className="Text-Paragraph-Link" href="https://pubmed.ncbi.nlm.nih.gov/38083026/" target={"_blank"}>Read on PubMed</a>
+            \&nbsp;&nbsp;<a className="Text-Paragraph-Link" href="https://doi.org/10.48550/arXiv.2304.14937" target={"_blank"}>Read on arXiv (no login required)</a>
+            \&nbsp;&nbsp;<a className="Text-Paragraph-Link" href="https://github.com/jamesbungay/cv-tremor-amplitude" target={"_blank"}>Code on GitHub</a>
+          </div>
+        }
+        imageObj={
+          <img alt="Icon of the first page of the paper."
+               src={require("../../assets/portfolio_dissertation.png")}
+               className="PortfolioImage" />
+        }
+      />
+      <br></br>
+      <PortfolioItem
+        bodyContent={
+          <div>
+            <p className="Text-SubTitle">
             CarbonCamera<br />iOS App
             </p>
             <p className="Text-Paragraph">
@@ -29,8 +53,8 @@ const Portfolio = () => {
             <p className="Text-Paragraph">
               Written in the Swift language, making use of UIKit and CoreML.
             </p>
-            <a className="Text-Paragraph-Link" href="https://github.com/jamesbungay/CarbonCamera">GitHub</a>
-            <a className="Text-Paragraph-Link" href="https://youtu.be/yVFjdLtYHbE">10 second video demo</a>
+            <a className="Text-Paragraph-Link" href="https://github.com/jamesbungay/CarbonCamera" target={"_blank"}>Code on GitHub</a>
+            \&nbsp;&nbsp;<a className="Text-Paragraph-Link" href="https://youtu.be/yVFjdLtYHbE" target={"_blank"}>10 second video demo</a>
           </div>
         }
         imageObj={
@@ -40,7 +64,10 @@ const Portfolio = () => {
         }
       />
 
-      <br /><p className="Text-Paragraph">Find more projects on GitHub.</p>
+      <br />
+      <p className="Text-Paragraph">Find more projects on <a className={"Text-Paragraph-Link-NoEmphasis"} href="https://github.com/jamesbungay" target={"_blank"}>GitHub</a>.</p>
+      <p className="Text-Paragraph">Read more about my experience on <a className={"Text-Paragraph-Link-NoEmphasis"} href="https://www.linkedin.com/in/james-bungay/" target={"_blank"}>LinkedIn</a>.</p>
+      <p className="Text-Paragraph">Email me at <a className={"Text-Paragraph-Link-NoEmphasis"} href="mailto:james@jamesjam.es">james@jamesjam.es</a>.</p>
       <br /><br />
 
       {/*
