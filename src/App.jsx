108cac1650c45cd54ea937995900e11f0d768279
James Bungay <jtb.github@gmail.com>
2025-12-07 19:36:07 +0000

    npm audit fix and remove react-ga

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGA from 'react-ga';
 import {
   BrowserRouter as Router,
   Switch,
@@ -7,18 +6,12 @@ import {
   Redirect
 } from 'react-router-dom';
 
-import RouteChangeTracker from './components/RouteChangeTracker';
-
 import Portfolio from './routes/Portfolio';
 import NotFound from './routes/NotFound';
 
 function App() {
-  const TRACKING_ID = "UA-193123795-1";
-  ReactGA.initialize(TRACKING_ID);
-  
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <RouteChangeTracker />
       <Switch>
         <Route path="/" exact>
           <Portfolio />
