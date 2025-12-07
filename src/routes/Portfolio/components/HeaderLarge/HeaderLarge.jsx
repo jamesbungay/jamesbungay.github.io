9c9dcc57987473afeb91181a2ca586a01c229a90
James Bungay <jtb.github@gmail.com>
2025-12-07 19:17:41 +0000

    Update header

--- src/routes/Portfolio/components/HeaderLarge/HeaderLarge.jsx
+++ src/routes/Portfolio/components/HeaderLarge/HeaderLarge.jsx
@@ -20,9 +20,9 @@ const HeaderLarge = () => {
       </div>
       <div className="LRmargin1rem">
         <div className="Text-Header">
-          <p>Graduate Software Engineer at the BBC.</p>
+          <p>Software Engineer at the BBC.</p>
           <p>University of Manchester <a className={"HeroLink"} href={"https://www.grammarly.com/blog/alumna-alumnae-alumni-alumnus/"} target={"_blank"}>alum</a>, with a <a className={"HeroLink"} href={"https://pubmed.ncbi.nlm.nih.gov/38083026/"} target={"_blank"}>published dissertation</a>.</p>
-          <p>Interested in environmentally sustainable software, health tech, iOS apps, and more.</p>
+          <p>Interested in environmentally sustainable software, health tech, iOS apps, and home IoT.</p>
         </div>
       </div>
     </div>
